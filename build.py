bc3755554be64a9d2ebb269d805d3e8654b52644
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2022-01-30 16:19:08 +0100

    updated build script

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ from mypyc.build import mypycify
 
 
 def build(setup_kwargs):
-    extensions = ["starlite/asgi.py"]
+    extensions = ["starlite/asgi.py", "starlite/parsers.py"]
     cmd_args = ["--ignore-missing-imports", "--config-file", "mypy.ini"]
     extensions.extend(cmd_args)
     setup_kwargs.update({"ext_modules": mypycify(extensions, verbose=True)})
